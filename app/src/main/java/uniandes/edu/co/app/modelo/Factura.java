340e24f2a35fb8de01f59e85bd0e09adcd4c756f
ncamargop <n.camargop@uniandes.edu.co>
2023-09-21 21:00:33 -0500

    data upload (crear y borrar tablas)

--- app/src/main/java/uniandes/edu/co/app/modelo/Factura.java
+++ app/src/main/java/uniandes/edu/co/app/modelo/Factura.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "bebedores")
+@Table(name = "facturas")
 public class Factura {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
